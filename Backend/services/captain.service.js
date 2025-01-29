b67e3dda040aa3b972ef96ca03278a5ec5b8b91e
Prithviraj <prithvirajkakatkar4@gmail.com>
2025-01-29 12:05:50 +0530

    captain profile, login, logout done

--- Backend/services/captain.service.js
+++ Backend/services/captain.service.js
@@ -27,3 +27,23 @@ module.exports.createCaptain = async ({
     })
     return captain;
 }
+// {
+//     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Nzk4M2RiMmJlOGQ4NWQ5ZDIyNGI2MGMiLCJpYXQiOjE3MzgwMzA1MTQsImV4cCI6MTczODExNjkxNH0.PjDzcSvLfWbprkmfcPrfTBh-L9zFwE8oRzfe4WgM8kk",
+//     "captain": {
+//         "fullname": {
+//             "firstname": "test_captain_firstname",
+//             "lastname": "test_captain_lastname"
+//         },
+//         "email": "test_captain@gmail.com",
+//         "password": "$2b$10$XyGRD2DLznn9i75fkOBKJ..q4NiQ.Gnr/jvdY9PlyjQsnt/zm9UMu",
+//         "status": "inactive",
+//         "vehicle": {
+//             "color": "red",
+//             "plate": "MP 04 XY 6234",
+//             "capacity": 3,
+//             "vehicleType": "auto"
+//         },
+//         "_id": "67983db2be8d85d9d224b60c",
+//         "__v": 0
+//     }
+// }
