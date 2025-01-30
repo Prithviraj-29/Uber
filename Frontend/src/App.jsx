6f9cdd9b336acecc1566515282589ab1ae36a91d
Prithviraj <prithvirajkakatkar4@gmail.com>
2025-01-30 07:10:12 +0530

    user and cap login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
 import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
         <Route path='/login' element={<UserLogin/>}/>
         <Route path='/signup' element={<UserSignup/>}/>
         <Route path='/captain-login' element={<CaptainLogin/>}/>
-        <Route path='/captain-signup' element={<CaptainLogin/>}/>
+        <Route path='/captain-signup' element={<CaptainSignup/>}/>
       </Routes>
     </div>
   )
--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const UserLogin = () => {
     const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('')
+    const [password,setPassword] = useState('');
+    const [userData,setUserData] = useState({});
+
+    const submitHandler = (e) =>{
+        e.preventDefault();
+        setUserData({ email, password });
+        console.log({ email, password }); // Log immediately
+        setEmail('');
+        setPassword('');
+    }
+
+   
 
 
 
@@ -11,7 +22,9 @@ const UserLogin = () => {
         <div className='p-7 h-screen flex flex-col justify-between'>
             <div>
                 <img className="w-16 mb-5" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" />
-                <form>
+                <form onSubmit={(e)=>{
+                    submitHandler(e)
+                }}>
                     <h3 className='text-lg font-medium mb-2'>What's your email</h3>
                     <input className='bg-[#eeeeee] mb-7 rounded px-4 py-2 w-full text-lg placeholder:text-base'
                         value={email} onChange={(e)=>{
@@ -33,7 +46,7 @@ const UserLogin = () => {
                 <p className='text-center'>New here? <Link to='/signup' className='text-blue-600'>Create new Accout</Link></p>
             </div>
             <div>
-                <button className='bg-[#10b461] text-white font-medium mb-7 rounded px-4 py-2 w-full text-sm placeholder:text-base '>Sign in as Captain</button>
+                <Link to="/captain-login" className='bg-[#10b461] flex items-center justify-center text-white font-medium mb-5 rounded px-4 py-2 w-full text-sm placeholder:text-base '>Sign in as Captain</Link>
             </div>
         </div>
     )
