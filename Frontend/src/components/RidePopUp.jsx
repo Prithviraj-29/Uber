c015915ee048968e980ba44a714f3cca280e04cc
Prithviraj <prithvirajkakatkar4@gmail.com>
2025-02-20 14:28:23 +0530

    Captain Confirm RIde Ui

--- Frontend/src/components/RidePopUp.jsx
+++ Frontend/src/components/RidePopUp.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const RidePopUp = (props) => {
-  return (
-    <div>
-        <h5 className='p-3 text-center w-[90%] absolute top-0' onClick={() => {
+    return (
+        <div>
+            <h5 className='p-3 text-center w-[90%] absolute top-0' onClick={() => {
                 props.setRidePopUpPanel(false)
             }}><i className="text-3xl text-gray-300 ri-arrow-down-wide-fill"></i></h5>
             <h3 className='text-2xl font-semibold mb-5'>New Ride Available!</h3>
@@ -25,29 +25,31 @@ const RidePopUp = (props) => {
                             <p className='text-gray-600 text-sm -mt-1'>kankariya Talab, Hyderabad</p>
                         </div>
                     </div>
-                    <div  className='flex items-center gap-5 p-2 border-b-2 border-gray-200'>
-                    <i className="text-lg ri-map-pin-range-line"></i>
+                    <div className='flex items-center gap-5 p-2 border-b-2 border-gray-200'>
+                        <i className="text-lg ri-map-pin-range-line"></i>
                         <div>
                             <h3 className='text-lg font-medium'>102-4/A</h3>
                             <p className='text-gray-600 text-sm -mt-1'>Road no.5 Jubliee hills, Hyderabad</p>
                         </div>
                     </div>
-                    <div  className='flex items-center gap-5 p-2 border-gray-400'>
-                    <i className="text-lg ri-currency-line"></i>
+                    <div className='flex items-center gap-5 p-2 border-gray-400'>
+                        <i className="text-lg ri-currency-line"></i>
                         <div>
                             <h3 className='text-lg font-medium'>₹ 74.45</h3>
                             <p className='text-gray-600 text-sm -mt-1'>Pay at destination</p>
                         </div></div>
                 </div>
-                <button onClick={()=>{
-                    props.setConfirmRidePopUpPanel(true)
-                }} className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg '>Accept</button>
-                <button onClick={()=>{
-                    props.setRidePopUpPanel(false)
-                }} className='w-full mt-5 bg-gray-300 text-black font-semibold p-2 rounded-lg '>Ignore</button>
+                <div className='flex justify-between items-center w-full px-8 mt-5 '>
+                    <button onClick={() => {
+                        props.setConfirmRidePopUpPanel(true)
+                    }} className= ' bg-green-600 text-white font-semibold p-3 px-10 rounded-lg '>Accept</button>
+                    <button onClick={() => {
+                        props.setRidePopUpPanel(false)
+                    }} className=' bg-gray-300 text-black font-semibold p-3 px-10 rounded-lg '>Ignore</button>
+                </div>
             </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default RidePopUp
